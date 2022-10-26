25d7989a6bbb8ae2c3215d5b0404527933c0625c
Alebelly Nemesis <anemesis@ev-h3.kzn.21-school.ru>
2022-10-26 15:07:20 +0300

    initial, add launch screen

---
+++ avito-test-2020/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  avito-test-2020
+//
+//  Created by Alebelly Nemesis on 10/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+    
+}
+
